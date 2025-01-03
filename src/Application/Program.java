da3b48f76762500833f56bb9db3d16145c113c2b
Fabio Filho <fabioffer6@gmail.com>
2025-01-03 19:34:07 -0300

    Seller entity Class

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,14 +1,19 @@
 package Application;
-
+import java.util.Date;
 
 import modelEntities.Department;
+import modelEntities.Seller;
+
 
 public class Program {
 
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+
+        Seller seller = new Seller(21, "Fabio", "Fabioffer6@gamil.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller);
 
     }
 }
