48d826b2890ffb14299c47a8724c53d0606f0e7f
Fabio Filho <fabioffer6@gmail.com>
2025-01-05 00:24:28 -0300

    IMPLEMENTAÇÃO TESTE DO DEPARTAMENTO

--- src/modelDao/DaoFactory.java
+++ src/modelDao/DaoFactory.java
@@ -1,11 +1,18 @@
 package modelDao;
 
 import Db.DB;
+import modelDao.DepatmentDao;
+import modelDao.SellerDao;
+import modelDaoImpl.DepartmentDaoJDBC;
 import modelDaoImpl.SellerDaoJDBC;
 
 public class DaoFactory {
 
     public static SellerDao createSellerDao() {
         return new SellerDaoJDBC(DB.getConnection());
+    }
+
+    public static DepatmentDao createDepartmentDao() {
+        return new DepartmentDaoJDBC(DB.getConnection());
     }
 }
